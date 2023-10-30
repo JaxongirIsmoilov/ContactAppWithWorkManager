3030c08cde1dee03192e53ac72abd2bae925fd21
User <1shavkatovich1@gmail.com>
2023-10-30 21:35:51 +0500

    usecasemodule

---
+++ app/src/main/java/uz/gita/jaxongir/contactappwithworkmanager/di/UseCaseModule.kt
@@ -0,0 +1,10 @@
+package uz.gita.jaxongir.contactappwithworkmanager.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface UseCaseModule {
+}
