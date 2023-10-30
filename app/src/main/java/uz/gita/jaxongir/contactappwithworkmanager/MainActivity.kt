72c6f71f4fd079b57ac076e6fd15fbeed0ecff03
Jakhongir Ismoilov <ismoilovj2018@gmail.com>
2023-10-30 21:13:24 +0500

    Main Activity Voyager implementation

--- app/src/main/java/uz/gita/jaxongir/contactappwithworkmanager/MainActivity.kt
+++ app/src/main/java/uz/gita/jaxongir/contactappwithworkmanager/MainActivity.kt
@@ -1,5 +1,6 @@
 package uz.gita.jaxongir.contactappwithworkmanager
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,22 +11,35 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import cafe.adriel.voyager.navigator.CurrentScreen
+import cafe.adriel.voyager.navigator.Navigator
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import uz.gita.jaxongir.contactappwithworkmanager.presentation.source.home.HomeScreen
+import uz.gita.jaxongir.contactappwithworkmanager.ui.navigator.NavigationHandler
 import uz.gita.jaxongir.contactappwithworkmanager.ui.theme.ContactAppWithWorkManagerTheme
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var navigationHandler: NavigationHandler
+    @SuppressLint("FlowOperatorInvokedInComposition", "CoroutineCreationDuringComposition")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ContactAppWithWorkManagerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+               Navigator(HomeScreen()) { navigator ->
+                   navigationHandler.navigationFlow
+                       .onEach {
+                           it.invoke(navigator)
+                       }.launchIn(lifecycleScope)
+                   CurrentScreen()
+               }
 
-                }
             }
         }
     }
