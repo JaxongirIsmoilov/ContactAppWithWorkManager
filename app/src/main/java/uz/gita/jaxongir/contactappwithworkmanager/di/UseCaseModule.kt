856a4f33af43491c5a4d77ac7ea3ac7599d7f11b
User <1shavkatovich1@gmail.com>
2023-10-30 22:11:39 +0500

    usecasemodule full

--- app/src/main/java/uz/gita/jaxongir/contactappwithworkmanager/di/UseCaseModule.kt
+++ app/src/main/java/uz/gita/jaxongir/contactappwithworkmanager/di/UseCaseModule.kt
@@ -4,8 +4,14 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import uz.gita.jaxongir.contactappwithworkmanager.domain.usecase.DeleteUseCase
 import uz.gita.jaxongir.contactappwithworkmanager.domain.usecase.GetAllContactDataUseCase
+import uz.gita.jaxongir.contactappwithworkmanager.domain.usecase.SaveAllDataUseCase
+import uz.gita.jaxongir.contactappwithworkmanager.domain.usecase.UpdateUseCase
+import uz.gita.jaxongir.contactappwithworkmanager.domain.usecase.impl.DeleteUseCaseImpl
 import uz.gita.jaxongir.contactappwithworkmanager.domain.usecase.impl.GetAllContactDataUseCaseImpl
+import uz.gita.jaxongir.contactappwithworkmanager.domain.usecase.impl.SaveAllDataUseCaseImpl
+import uz.gita.jaxongir.contactappwithworkmanager.domain.usecase.impl.UpdateUseCaseImpl
 import javax.inject.Singleton
 
 @Module
@@ -13,6 +19,15 @@ import javax.inject.Singleton
 interface UseCaseModule {
 
     @[Binds Singleton]
-    fun bindGetAllContactDataUseCase(impl : GetAllContactDataUseCaseImpl) : GetAllContactDataUseCase
+    fun bindGetAllContactDataUseCase(impl: GetAllContactDataUseCaseImpl): GetAllContactDataUseCase
+
+    @[Binds Singleton]
+    fun bindUpdateContactUseCase(impl: UpdateUseCaseImpl): UpdateUseCase
+
+    @[Binds Singleton]
+    fun bindDeleteContactUseCase(impl: DeleteUseCaseImpl): DeleteUseCase
+
+    @[Binds Singleton]
+    fun bindSaveAllDataUseCase(impl: SaveAllDataUseCaseImpl): SaveAllDataUseCase
 
 }
