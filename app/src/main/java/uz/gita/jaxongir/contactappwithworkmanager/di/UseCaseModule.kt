42257bf6a104b184f600706b26fc4854fae873a9
umidjonakramov699 <umidjonakramov699@gmail.com>
2023-10-30 21:42:28 +0500

    add ContactDao delete add DeleteUseCase delete add DeleteUseCaseImpl delete add Repository delete add RepositoryImpl delete

--- app/src/main/java/uz/gita/jaxongir/contactappwithworkmanager/di/UseCaseModule.kt
+++ app/src/main/java/uz/gita/jaxongir/contactappwithworkmanager/di/UseCaseModule.kt
@@ -1,10 +1,18 @@
 package uz.gita.jaxongir.contactappwithworkmanager.di
 
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import uz.gita.jaxongir.contactappwithworkmanager.domain.usecase.GetAllContactDataUseCase
+import uz.gita.jaxongir.contactappwithworkmanager.domain.usecase.impl.GetAllContactDataUseCaseImpl
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 interface UseCaseModule {
+
+    @[Binds Singleton]
+    fun bindGetAllContactDataUseCase(impl : GetAllContactDataUseCaseImpl) : GetAllContactDataUseCase
+
 }
